bead7752da7ce5de8634a25bee355cab7d0f75ee
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-27 18:56:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 
-
-
 def reverse_each_word(string)
-sentence1.split(" ")
-  sentence1.each do |string|
-    reverse
+  sentence1.split(" ")
+    sentence1.each do |string|
+      reverse
   end
 end
